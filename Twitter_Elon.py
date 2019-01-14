d460903ddb7e92262fe86ec58ebdb598cebfd278
Joerg-ffs <40400868+Joerg-ffs@users.noreply.github.com>
2019-01-13 22:21:48 -0500

    Implemented plotting of sentiment

--- Twitter_Elon.py
+++ Twitter_Elon.py
@@ -110,10 +110,10 @@ class TweetAnalyzer():
       
 
     def tweets_to_data_frame(self, tweets):
-        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+        df = pd.DataFrame(data=[tweet.full_text for tweet in tweets], columns=['tweets'])
 
         df['id'] = np.array([tweet.id for tweet in tweets])
-        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['len'] = np.array([len(tweet.full_text) for tweet in tweets])
         df['date'] = np.array([tweet.created_at for tweet in tweets])
         df['source'] = np.array([tweet.source for tweet in tweets])
         df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
@@ -129,9 +129,12 @@ if __name__ == '__main__':
 
     api = twitter_client.get_twitter_client_api()
 
-    tweets = api.user_timeline(screen_name="elonmusk", count=200, exclude_replies=True)
+    tweets = api.user_timeline(screen_name="elonmusk", count=200, exclude_replies=True, include_rts = False, tweet_mode="extended", )
     
     df = tweet_analyzer.tweets_to_data_frame(tweets)
     df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
-    #print(tweets[0].retweeted)
-    print(df.head(200))
+
+    df.to_csv('C:\\Users\Joerg\\Desktop\\elon_tweets.csv', index=False)
+
+    plt.plot(df['sentiment'])
+    plt.show()
